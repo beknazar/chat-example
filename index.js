6b0c81030e3dd72128311be2110b1f98af2c1cc5
beknazar <beknazar23@gmail.com>
2015-05-27 00:49:57 +0900

    major changes

--- index.js
+++ index.js
@@ -22,52 +22,94 @@ app.set('view engine', 'jade');
 // variables of app
 
 app.set('name', 'Simple chat by Bek');
-var nicknames = [];
+
+// Chatroom
+
+// usernames which are currently connected to the chat
+
+var usernames = {};
+var numUsers = 0;
+
 
 /********
  * Routes
  ********/
 
 app.get('/', function(req, res){
-  // res.sendFile(__dirname + '/index.html');
-  res.render('home', {
-  	name: app.get('name')
-  });
+	res.render('home', {
+		name: app.get('name'),
+		nicks: usernames
+  	});
 });
 
 app.get('/script.js', function(req, res){
   res.sendFile(__dirname + '/script.js');
 });
 
+
 /********
  * Socket configuration
  ********/
 
-io.on('connection', function(socket){
-	// var nick = prompt("Please enter your nickname", "");
-	// while (nicknames.indexOf (nick) != -1) {
-	// 	nick = prompt("Oops, it's already taken. Please enter different nickname", "");
-	// }
-	// nicknames.push(nick);
-	
-	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
+io.on('connection', function (socket) {
+	var addedUser = false;
+
+	// when the client emits 'new message', this listens and executs
+	socket.on('new message', function (data) {
+		// we tell the client to execute 'new message'
+		socket.broadcast.emit('new message', data);
+		console.log(socket.nick);
 	});
 
-	socket.on('disconnect', function(msg) {
-		io.emit('user disconnected', 'user is disconnected');
+	socket.on('add user', function (nick) {
+		// store the nick in the socket session
+		socket.nick = nick;
+		// add the clients nick to the global list
+		usernames[nick] = nick;
+		console.log(nick);
+		++numUsers;
+		addedUser = true;
+		socket.emit('login', {
+			numUsers: numUsers
+		});
+		// echo globally (all clients) that a person has connected
+		io.sockets.emit('user joined', {
+			nick: socket.nick,
+			numUsers: numUsers
+		});
 	});
 
-	socket.on('user try', function(msg) {
-		if (nicknames.indexOf(msg) != -1) {
-			io.emit('user exists', '');
-		}
-		else {
-			nicknames.push(msg);
-			io.emit('user connected', msg + ' has entered the chat');
+	// when the client emits 'typing', we broadcast it to others
+	socket.on('typing', function () {
+		socket.broadcast.emit('typing', {
+			nick: socket.nick
+		});
+	});
+
+	// when the client 'stops typing'
+	socket.on('stop typing', function () {
+		socket.broadcast.emit('stop typing', {
+			nick: socket.nick
+		});
+	});
+
+	// when user disconnects
+	socket.on('disconnect', function () {
+		// remove the nick from global list
+		if (addedUser) {
+			delete usernames[socket.nick];
+			--numUsers;
+
+			// echo it globally
+			io.sockets.emit('user left', {
+				nick: socket.nick,
+				numUsers: numUsers
+			});
 		}
 	});
 });
+
+
 
 /**************
  * Start Server
--- script.js
+++ script.js
@@ -1,45 +1,286 @@
-var socket = io();
+$(function() {
+	var FADE_TIME = 150; // ms
+	var TYPING_TIMER_LENGTH = 400; // ms
+	var COLORS = [
+		'#e21400', '#91580f', '#f8a700', '#f78b00',
+		'#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
+		'#3b88eb', '#3824aa', '#a700ff', '#d300e7'
+	];
 
-$('#register').submit(function() {
-	$('#register input').val();
-	socket.on('user exists', function(msg) {
-		$('#labelError').toggleClass('hidden ');
+	// Initialize variables
+	var $window = $(window);
+	var $nickInput = $('.nickInput');
+	var $messages = $('.messages');
+	var $inputMessage = $('.inputMessage');
+	var $usersList = $('.users');
+
+	var $loginArea = $('.loginArea');
+	var $chatArea = $('.chatArea');
+
+	// Prompt for setting a nickname
+	var nick;
+	var connected = false;
+	var typing = false;
+	var lastTypingTime;
+	var $currentInput = $nickInput.focus();
+
+	var socket = io();
+
+	function addParticipantsMessage (data) {
+		var message = '';
+		if (data.numUsers === 1) {
+			message += "there is 1 participant";
+		} else {
+			message += "there are " + data.numUsers + " participants";
+		}
+		log(message);
+	}
+
+	// set the client's nickname
+	function setNick () {
+		nick = cleanInput($nickInput.val().trim());
+
+		if (nick) {
+			$loginArea.fadeOut();
+			$chatArea.show();
+			$loginArea.off('click');
+			$currentInput = $inputMessage.focus();
+
+			// Tell the server your nick
+			socket.emit('add user', nick);
+		}
+	}
+
+	// Send a chat message
+	function sendMessage() {
+		var message = $inputMessage.val();
+
+		// Prevent markup from being injected into the message
+		message = cleanInput(message);
+		if (message && connected) {
+			// console.log(message);
+			$inputMessage.val('');
+			addChatMessage({
+				nick: nick,
+				message: message
+			});
+			data = {nick: nick, message: message};
+			// tell server to execute 'new message' and send along one parameter
+			socket.emit('new message', data);
+		}
+	}
+
+	// Log a message
+	function log (message, options) {
+		var $el = $('<li>').addClass('log').text(message);
+		addMessageElement($el, options);
+	}
+
+	// update users list
+	function updateUsersList (nick, option) {
+		// console.log(nick + ' : ' + option);
+		if (option) {
+			var $userDiv = $('<li class="user"/>')
+				.text(nick)
+				.data('nick', nick);
+			$usersList.append($userDiv);
+		} else {
+			var $target = $('.user').filter(function (i) {
+				return $(this).data('nick') === nick;
+			});
+			$target.fadeOut(function () {
+				$(this).remove();
+			});
+		}
+	}
+
+	// Add the visual chat message to the message list
+	function addChatMessage (data, options) {
+		// DOn't fade the message if there is an 'X was typing'
+		var $typingMessages = getTypingMessages(data);
+		options = options || {};
+		if ($typingMessages.length !== 0) {
+			options.fade = false;
+			$typingMessages.remove();
+		}
+
+		var $nickDiv = $('<span class="nick"/>')
+			.text(data.nick)
+			.css('color', getNickColor(data.nick));
+		var $messageBodyDiv = $('<span class ="messageBody">')
+			.text(data.message);
+		console.log(data.message);
+		var typingClass = data.typing ? 'typing' : '';
+		var $messageDiv = $('<li class "message"/>')
+			.data('nick', data.nick)
+			.addClass(typingClass)
+			.append($nickDiv, $messageBodyDiv);
+
+		addMessageElement($messageDiv, options);
+	}
+
+
+	// Adds the visual chat typing message
+	function addChatTyping (data) {
+		data.typing = true;
+		data.message = 'is typing';
+		addChatMessage(data);
+	}
+
+	// Removes the visual chat typing message
+	function removeChatTyping (data) {
+		// console.log(data);
+		getTypingMessages(data).fadeOut(function () {
+			$(this).remove();
+		});
+	}
+
+	function addMessageElement(el, options) {
+		var $el = $(el);
+
+		// Setup default options
+		if (!options) {
+			options = {};
+		}
+		if (typeof options.fade === 'undefined') {
+			options.fade = true;
+		}
+		if (typeof options.prepend === 'undefined') {
+			options.prepend = false;
+		}
+
+		// Aply options
+		if (options.fade) {
+			$el.hide().fadeIn(FADE_TIME);
+		}
+		if (options.prepend) {
+			$messages.prepend($el);
+		} else {
+			$messages.append($el);
+		}
+		$messages[0].scrollTop = $messages[0].scrollHeight;
+	}
+
+	// Prevents input from having injected markup
+	function cleanInput (input) {
+		return $('<div/>').text(input).text();
+	}
+
+	// Update the typing event
+	function updateTyping () {
+		if (connected) {
+			if (!typing) {
+				typing = true;
+				socket.emit('typing', {nick: socket.nick});
+			}
+			lastTypingTime = (new Date()).getTime();
+
+			setTimeout(function () {
+				var typingTimer = (new Date()).getTime();
+				var timeDiff = typingTimer - lastTypingTime;
+				if (timeDiff >= TYPING_TIMER_LENGTH && typing) {
+					socket.emit('stop typing');
+					typing = false;
+				}
+			}, TYPING_TIMER_LENGTH);
+		}
+	}
+
+	// Get the 'X is typing' message of a user
+	function getTypingMessages (data) {
+		return $('.typing').filter(function (i) {
+			return $(this).data('nick') === data.nick;
+		});
+	}
+
+	// Gets the color of a username through our hash function
+	function getNickColor (nick) {
+		// compute the has code
+		var hash = 7;
+		for (var i = 0; i < nick.length; ++i) {
+			hash = nick.charCodeAt(i) + (hash << 5) - hash;
+		}
+		var index = Math.abs(hash % COLORS.length);
+		return COLORS[index];
+	}
+
+	// Keyboard Events
+
+	$window.keydown(function (event) {
+		// Auto focus the current input when is typed
+		if (!(event.ctrlKey || event.metaKey || event.altKey)) {
+			$currentInput.focus();
+		}
+		// when the client hits ENTER 
+		if (event.which === 13) {
+			if (nick) {
+				// console.log(nick);
+				sendMessage();
+				socket.emit('stop typing');
+				typing = false;
+			} else {
+				setNick();
+			}
+		}
 	});
-	$('#nickGet').hide('slow');
-	return false;
-});
 
-$(document).ready(function() {
-	$('#messages').scrollTop($('#messages')[0].scrollHeight);
-});
+	$inputMessage.on('input', function () {
+		updateTyping();
+	})
 
-$('.inputArea').submit(function(){
-	socket.emit('chat message', $('#m').val());
-	$('#m').val('');
-	return false;
-});
+	// Click events
 
-
-function appendNewLi (msg) {
-	$('#messages').scrollTop($('#messages')[0].scrollHeight);
-	$('#messages').append($('<li>').text(msg));
-	$('#messages').show('slow', function() {
-		$('messages').scrollTop($('#messages')[0].scrollHeight);
+	// Focus input when clicking anywhere on login page
+	$loginArea.click(function () {
+		$currentInput.focus();
 	});
 
-	$("#messages").animate({
-    	scrollTop: $("#messages")[0].scrollHeight
- 	},'slow');
-}
+	// Focus input when clicking on the message input's border
+	$inputMessage.click(function () {
+		$inputMessage.focus();
+	});
 
-socket.on('chat message', function(msg) {	
-	appendNewLi(msg);
-});
+	// Socket events
 
-socket.on('user connected', function(msg) {
-	appendNewLi(msg);
-});
+	// Whenever the server emits 'login', log the login message
+	socket.on('login', function (data) {
+	    connected = true;
+	    // Display the welcome message
+	    var message = "Welcome to Beks Chat – ";
+	    log(message, {
+			prepend: true
+	    });
+	    addParticipantsMessage(data);
+	});
 
-socket.on('user disconnected', function(msg) {
-	appendNewLi(msg);
+	// Whenever the server emits 'new message', update the chat body
+	socket.on('new message', function (data) {
+		addChatMessage(data);
+	});
+
+	// Whenever the server emits 'user joined', log it in the chat body
+	socket.on('user joined', function (data) {
+		log(data.nick + ' joined');
+		addParticipantsMessage(data);
+		updateUsersList(data.nick, true);
+	});
+
+	// Whenever the server emits 'user left', log it in the chat body
+	socket.on('user left', function (data) {
+		log(data.nick + ' left');
+		addParticipantsMessage(data);
+		removeChatTyping(data);
+		updateUsersList(data.nick, false);
+	});
+
+	// Whenever the server emits 'typing', show the typing message
+	socket.on('typing', function (data) {
+		addChatTyping(data);
+	});
+
+	// Whenever the server emits 'stop typing', kill the typing message
+	socket.on('stop typing', function (data) {
+		removeChatTyping(data);
+	});
+
 });
